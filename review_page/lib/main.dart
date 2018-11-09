f5e7b3924aec69050b2f67f5f618f86c952570d9
rshrc <rishieric91@gmail.com>
2018-11-09 14:30:03 +0530

    90% done with the Review Page : HardCoded

--- review_page/lib/main.dart
+++ review_page/lib/main.dart
@@ -15,26 +15,229 @@ class _MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: new Text("Review Page"),
         ),
-        body: new ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, int index) {
-            return new Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: CircleAvatar(
-                      child: Text("R"),
-                    ),
-                    title: Text('Rishi Banerjee'),
-                    subtitle: Text(
-                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
-                    trailing: Icon(Icons.thumb_up),
+        body: new ListView(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
                   ),
-                ],
-              ),
-            );
-          },
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("AB"),
+                  ),
+                  title: Text('Akshit Banta'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Some Google User"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("PJ"),
+                  ),
+                  title: Text('Pratik Jain'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Ateek Ujjwal"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text("R"),
+                  ),
+                  title: Text('Rishi Banerjee'),
+                  subtitle: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'),
+                  trailing: Icon(Icons.thumb_up),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 54.0),
+                  child: ListTile(
+                    title: Text("Apoorv Jain"),
+                    subtitle: Text("This is the Reply"),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       );
     }
